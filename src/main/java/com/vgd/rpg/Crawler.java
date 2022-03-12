78b23181285c427c56cf9984c53fa54e21e6966d
Vishal Gowda <cartmanboy1991@gmail.com>
2022-03-12 14:58:37 +0000

    Define crawler

--- src/main/java/com/vgd/rpg/Crawler.java
+++ src/main/java/com/vgd/rpg/Crawler.java
@@ -7,13 +7,22 @@ import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
 import akka.japi.Pair;
 import akka.stream.Attributes;
-import akka.stream.BoundedSourceQueue;
+import akka.stream.ClosedShape;
 import akka.stream.OverflowStrategy;
+import akka.stream.UniformFanInShape;
+import akka.stream.UniformFanOutShape;
+import akka.stream.javadsl.Broadcast;
+import akka.stream.javadsl.Concat;
 import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.GraphDSL;
+import akka.stream.javadsl.RunnableGraph;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
-import akka.stream.javadsl.SourceQueueWithComplete;
 import java.time.Duration;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.regex.Pattern;
@@ -25,56 +34,23 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Crawler {
-
-  public static final int crawlDepthLimit = 6;
   public static final String seedUrl = "https://en.wikipedia.org/wiki/Main_Page";
   private static ActorSystem actorSystem = ActorSystem.create();
   private static Http http = Http.get(actorSystem);
   private static Pattern hrefP = Pattern.compile("href=\"(.*?)\"");
 
   public static void main(String[] args) {
-    Source<Pair<String, Long>, NotUsed> urlSrc = Source.single(
-        Pair.create(seedUrl, 1L));
+    RunnableGraph<NotUsed> runnableGraph = crawlerWithSeed(new CrawlSpec(seedUrl, 1L));
 
-    Pair<BoundedSourceQueue<Pair<String, Long>>, Source<Pair<String, Long>, NotUsed>> pair = Source.<Pair<String, Long>>queue(
-            64)
-        .preMaterialize(actorSystem);
-    BoundedSourceQueue<Pair<String, Long>> queue = pair.first();
-    Source<Pair<String, Long>, NotUsed> qSrc = pair.second();
-
-    urlSrc.concat(qSrc)
-        .log("crawling")
-        .filter(uriAndDepth -> uriAndDepth.second() < 8)
-        .mapAsyncUnordered(32, urlAndDepth -> Crawler.processUrl(urlAndDepth)
-            .thenApply(body -> PageDescriptor.builder()
-                .body(body)
-                .uri(urlAndDepth.first())
-                .depth(urlAndDepth.second())
-                .build()))
-        .alsoTo(Flow.of(PageDescriptor.class)
-            .log("page-stats",
-                pd -> String.format("uri: %s, num-words: %d, crawl-depth: %d", pd.getUri(), pd.getBody().length(), pd.getDepth()))
-            .to(Sink.ignore()))
-        .mapConcat(
-            pd -> hrefP.matcher(pd.getBody())
-                .results()
-                .map(mr -> mr.group(1))
-                .filter(href -> href.startsWith("http"))
-                .map(href -> Pair.create(pd, href))
-                .collect(Collectors.toList()))
-        .map(pdAndHref -> Pair.create(pdAndHref.second(),
-              pdAndHref.first().getDepth() + 1))
-//        .log("enqueuing")
-        .map(queue::offer)
-        .to(Sink.ignore()).withAttributes(Attributes.createLogLevels(Logging.InfoLevel()))
+    runnableGraph.withAttributes(Attributes.createLogLevels(Logging.InfoLevel()))
         .run(actorSystem);
-
   }
 
-  private static CompletionStage<String> processUrl(Pair<String, Long> urlAndDepth) {
+
+  private static CompletionStage<String> processUrl(String url) {
     try {
-      return http.singleRequest(HttpRequest.create().withUri(urlAndDepth.first()))
-          .thenCompose(resp -> resp.entity().toStrict(512, actorSystem))
+      return http.singleRequest(HttpRequest.create().withUri(url))
+          .thenCompose(resp -> resp.entity().toStrict(1024, actorSystem))
           .thenApply(resp -> resp.getData().utf8String())
           .exceptionally(err -> "");
     } catch (RuntimeException re) {
@@ -82,12 +58,83 @@ public class Crawler {
     }
   }
 
+  @SuppressWarnings("unchecked")
+  private static RunnableGraph<NotUsed> crawlerWithSeed(CrawlSpec seed) {
+    Source<CrawlSpec, NotUsed> seedSrc = Source.single(seed);
+    Flow<CrawlSpec, PageDescriptor, NotUsed> procFlo = Flow.of(CrawlSpec.class)
+        .zipWithIndex()
+        .map(Pair::first)
+        .throttle(1, Duration.ofMillis(100))
+        .statefulMapConcat(() -> {
+          Set<String> processed = new HashSet<>();
+
+          return cs -> {
+            if (processed.contains(cs.getUrl())) {
+              return Collections.emptyList();
+            }
+
+            processed.add(cs.getUrl());
+            return Collections.singletonList(cs);
+          };
+        })
+        .mapAsync(32, cspec -> processUrl(cspec.getUrl())
+            .thenApply(body -> PageDescriptor.builder()
+                .uri(cspec.getUrl())
+                .depth(cspec.getDepth())
+                .body(body)
+                .refs(hrefP.matcher(body)
+                    .results()
+                    .map(mr -> mr.group(1))
+                    .filter(href -> href.startsWith("http"))
+                    .collect(Collectors.toList()))
+                .build()));
+
+    Sink<PageDescriptor, NotUsed> pageSink = Flow.of(PageDescriptor.class)
+        .log("page-stats",
+            pd -> String.format("uri: %s, num-words: %d, crawl-depth: %d", pd.getUri(),
+                pd.getBody().length(), pd.getDepth()))
+        .to(Sink.ignore());
+
+    return RunnableGraph.fromGraph(
+        GraphDSL.create(b -> {
+          UniformFanInShape<CrawlSpec, CrawlSpec> fanin = b.add(Concat.create());
+          UniformFanOutShape<PageDescriptor, PageDescriptor> bcast = b.add(Broadcast.create(2));
+
+          b.from(b.add(seedSrc)).toFanIn(fanin);
+
+          b.from(fanin)
+              .via(b.add(procFlo))
+              .toFanOut(bcast);
+
+          b.from(bcast).to(b.add(pageSink));
+          b.from(bcast)
+              .via(
+                  b.add(Flow.of(PageDescriptor.class)
+                      .mapConcat(pd -> pd.getRefs().stream()
+                          .map(ref -> new CrawlSpec(ref, pd.getDepth() + 1))
+                          .collect(Collectors.toList()))
+                      .buffer(99999999, OverflowStrategy.backpressure())))
+              .toFanIn(fanin);
+
+          return ClosedShape.getInstance();
+        }));
+  }
+
+  @Value
+  static class CrawlSpec {
+
+    String url;
+    Long depth;
+  }
+
   @Value
   @Builder(toBuilder = true)
   @ToString(exclude = {"body"})
   static class PageDescriptor {
+
     String uri;
     Long depth;
     String body;
+    List<String> refs;
   }
 }
